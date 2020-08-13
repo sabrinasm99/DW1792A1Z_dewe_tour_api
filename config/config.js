076f1443a370e68dd06f20bad305f9892ddbba77
Sabrina Sri Mulyani <sabrinasrimulyani@gmail.com>
2020-08-14 02:18:10 +0700

    users

--- config/config.js
+++ config/config.js
@@ -1,24 +1,24 @@
 module.exports = {
-  "development": {
-    "username": "root",
-    "password": process.env.DB_PASS,
-    "database": "dewetour",
-    "host": "188.166.217.100",
-    "port": 8000,
-    "dialect": "mysql"
+  development: {
+    username: "root",
+    password: process.env.DB_PASS,
+    database: "dewetour",
+    host: "188.166.217.100",
+    port: 8000,
+    dialect: "mysql",
   },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql",
   },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  }
-}
+  production: {
+    username: "root",
+    password: null,
+    database: "database_production",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+};
