d4364b89acde4df23ef6985becc3d51ed2217110
Sabrina Sri Mulyani <sabrinasrimulyani@gmail.com>
2020-08-16 03:50:03 +0700

    trip

--- routes/routerv1.js
+++ routes/routerv1.js
@@ -13,15 +13,32 @@ const {
   addCountry,
   editCountry,
 } = require("../controller/country");
+const {
+    readTrips,
+    readDetailTrip,
+    addTrip,
+    editTrip,
+    deleteTrip,
+  } = require("../controller/trip");
 const { authenticated } = require("../middleware/auth");
 
+// User
 router.post("/register", register);
 router.post("/login", login);
 router.get("/users", readUsers);
 router.delete("/users/:id", deleteUser);
+
+// Country
 router.get("/country", readCountries);
 router.get("/country/:id", readDetailCountry);
 router.post("/country", authenticated, addCountry);
 router.patch("/country/:id", authenticated, editCountry);
+
+// Trip
+router.get("/trip", readTrips);
+router.get("/trip/:id", readDetailTrip);
+router.post("/trip", authenticated, fileUpload(), addTrip);
+router.patch("/trip/:id", authenticated, fileUpload(), editTrip);
+router.delete("/trip/:id", authenticated, deleteTrip)
 
 module.exports = router;
